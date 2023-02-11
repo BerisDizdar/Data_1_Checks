8522331e9459f43d507fd8be73c3bb0bbb54bcb4
Beris Dizdar <Berisd2010@gmail.com>
2023-02-11 16:00:21 -0500

    First assignment

---
+++ Knowledge_Check1.py
@@ -0,0 +1,17 @@
+# Statement that prints 'hello world'.
+print('Hello World!') 
+
+
+# List populated with several values. Then print one of those values.
+States_Surrounding_KY = ['Illinois', 'Ohio', 'Indiana', 'Tennessee', 'Virginia', 'West Virginia', 'Missouri'] 
+print(States_Surrounding_KY[1]) 
+
+
+# Dictionary populated with two keys and two values. Print one value, just like above.
+Operating_Systems = {'Microsoft': 'Windows', 'Apple': 'macOS'} 
+print(Operating_Systems['Apple']) 
+
+
+#Tuple with four values, print one of them.
+Popular_Smartphones = ('Google Pixel', 'Samsung Galax S', 'Apple iPhone', 'Motorola Razr') 
+print(Popular_Smartphones[2]) 
