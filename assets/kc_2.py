10a30d8336e0d82d190bcee31d47325336e5b43e
Beris Dizdar <Berisd2010@gmail.com>
2023-03-07 14:49:32 -0500

    Knoldedge Check 2

--- assets/kc_2.py
+++ assets/kc_2.py
@@ -9,20 +9,20 @@ from matplotlib import pyplot as plt
 # Create DataFrame (df) 
 df_Louisville = pd.read_csv('assets/Louisville_Weather_CSV.csv')
 
-print(df_Louisville.head()) 
+print(df_Louisville.head()) # Display the first five rows from the data.
 
 # DateTime
-df_Louisville['DATE'] = pd.to_datetime(df_Louisville['DATE'])
+df_Louisville['DATE'] = pd.to_datetime(df_Louisville['DATE'])   # Datetime module used to manipulate data using the 'DATE' column.
 
-Louisville_monthly_mean = df_Louisville.groupby(df_Louisville.DATE.dt.month)[['TMAX', 'TAVG', 'TMIN']].mean() 
+Louisville_monthly_mean = df_Louisville.groupby(df_Louisville.DATE.dt.month)[['TMAX', 'TAVG', 'TMIN']].mean()   # The mean() function calculates the average of the 'TMAX' 'TMIN' and 'TAVG' columns for each month.
 
-print(round(Louisville_monthly_mean, 2)) 
+print(round(Louisville_monthly_mean, 2))    # The 'round' function rounds the data, in this case it was set to allow two decimal points.
 
 # Graph data
 plt.plot(Louisville_monthly_mean, 'o')  
 plt.xlabel('Month')
-plt.ylabel('Temperature in Fahrenheit') 
-plt.title('Monthly Temperature Highs, Lows, and Averages, for Louisville KY')
+plt.ylabel('Temperature (Fahrenheit)') 
+plt.title(' 2022 Mean Monthly Temperature Highs, Lows, and Averages, for Louisville KY')
 plt.legend(Louisville_monthly_mean) 
 
-print(plt.show())  
+print(plt.show())   # Graph displays the monthly mean high, low, and average temperature for Louisville Ky, for the year 2022.
